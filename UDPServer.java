694419d60005da860302b172aaa93ef8885dc373
bobobo00 <48381156+bobobo00@users.noreply.github.com>
2019-11-15 22:35:43 +0800

    模拟接收方-实现多次接收

--- UDPServer.java
+++ UDPServer.java
@@ -1,5 +1,10 @@
 package UDP;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
@@ -19,16 +24,30 @@ public class UDPServer {
 	public static void main(String[] args) throws Exception {
 		System.out.println("接收方启动中....");
 		//使用DatagramSocket 指定端口，创建接收端
-		DatagramSocket server=new DatagramSocket(9999);
-		//准备容器，封装成DatagramPacket包裹
-		byte[] datas=new byte[1024*60];
-		DatagramPacket packet=new DatagramPacket(datas,0,datas.length);
-		//阻塞式接收包裹
-		server.receive(packet);
+		DatagramSocket server=new DatagramSocket(6666);
+		
 		//分析数据
-		byte[] b=packet.getData();
-		System.out.println(new String(b));
+		/*ByteArrayInputStream is=new ByteArrayInputStream(packet.getData());
+		OutputStream os=new BufferedOutputStream(new FileOutputStream(new File("copy.jpg")));
+		byte[] flush=new byte[1024];
+		while(is.read(flush)!=-1) {
+			os.write(flush);
+		}
+		os.flush();*/
+		while(true) {
+			//准备容器，封装成DatagramPacket包裹
+			byte[] datas=new byte[1024*60];
+			DatagramPacket packet=new DatagramPacket(datas,0,datas.length);
+			//阻塞式接收包裹
+			server.receive(packet);
+			String str=new String(packet.getData(),0,packet.getLength());
+			System.out.println(str);
+			if(str.equals("bye")) {
+				break;
+			}
+		}
 		//释放资源
+		System.out.println("接收完毕");
 		server.close();
 		
 	}
